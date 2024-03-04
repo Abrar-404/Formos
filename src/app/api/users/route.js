6b833561d9b8671b1a902ee36bb3001e158b25e3
Sahil Abrar <sahilabrar444@gmail.com>
2024-03-04 13:32:12 +0600

    added form to home

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,8 +1,8 @@
-import { connectionSRT } from "@/lib/db";
-import mongoose from "mongoose";
-import { NextResponse } from "next/server";
+import { connectionSRT } from '@/lib/db';
+import mongoose from 'mongoose';
+import { NextResponse } from 'next/server';
 
 export async function GET() {
- await mongoose.connect(connectionSRT)
-  return NextResponse.json({result:true})
+  //  await mongoose.connect(connectionSRT)
+  return NextResponse.json({ result: true });
 }
